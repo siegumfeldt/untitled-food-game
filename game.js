b36ab35d0a20ca71270d1aff090ac302dadf5be6
Frederik Siegumfeldt <frs@ekf.dk>
2022-01-02 23:43:27 +0100

    enkel animation

--- game.js
+++ game.js
@@ -5,21 +5,32 @@ const Application = PIXI.Application,
     Rectangle = PIXI.Rectangle,
     Sprite = PIXI.Sprite;
 
+
+PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+
 const app = new PIXI.Application({
     width: 400,
     height: 400,
-    antialias: true
+    antialias: false
 });
 
 app.renderer.backgroundColor = 0x000000;
 
-const ingredientTextures = {}
+const ingredientTextures = {};
+const lanes = [];
 
-function loadIngredientTextures(name){
-  const sheet = resources["assets/" + name + ".png"].texture;
+function addLane(number) {
+  const lane = new PIXI.Container();
+  lane.x = 100 + number*200;
+  lanes[number] = lane;
+  app.stage.addChild(lane);
+}
+
+function loadIngredientTextures(name, row){
+  const sheet = resources["assets/ingredients.png"].texture;
   ingredientTextures[name] = new Array();
-  for (let i = 0; i < 16; i++) {
-    ingredientTextures[name].push(new Texture(sheet, new Rectangle(16*i, 0, 16, 16)));   
+  for (let col = 0; col < 16; col++) {
+    ingredientTextures[name].push(new Texture(sheet, new Rectangle(col*16, row*16, 16, 16)));   
   }
 }
 
@@ -27,21 +38,34 @@ function getIngredient(name) {
   animatedSprite = new PIXI.AnimatedSprite(ingredientTextures[name]);
   animatedSprite.animationSpeed = 0.2;
   animatedSprite.play();
+  animatedSprite.scale = new PIXI.Point(2, 2);
+  animatedSprite.name = name;
   return animatedSprite;
 }
 
 
 function gameLoop(delta) {
-  
+  lanes.forEach(lane => {
+    lane.children.forEach(food => {
+      if(food.y > 300) {
+        food.destroy();
+      } else {
+        food.y += delta;
+      }
+    })
+  })
 }
 
 function setup() {
-  loadIngredientTextures("meat");
-  const s = getIngredient("meat");
-  app.stage.addChild(s);
+  loadIngredientTextures("meat", 0);
+  loadIngredientTextures("top-bun", 1);
+  addLane(0);
+
+  lanes[0].addChild(getIngredient("meat"));
+
   app.ticker.add((delta) => gameLoop(delta));
 }
 
 
-loader.add("assets/meat.png")
+loader.add("assets/ingredients.png")
       .load(setup);
